7602ac3de28f83a75eea4b6a136f33473a9eff40
James Zhao <hantian@google.com>
2020-08-15 22:03:39 -0400

    save to date 8/15

--- host/btle-tools/src/btle_blade.c
+++ host/btle-tools/src/btle_blade.c
@@ -1,39 +1,39 @@
 #include "btle_blade.h"
 
-#include <ctype.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <getopt.h>
 #include <libbladeRF.h>
-#include <math.h>
-#include <netinet/in.h>
+
+// System includes
 #include <signal.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <unistd.h>
+/* #include <ctype.h> */
+/* #include <errno.h> */
+/* #include <fcntl.h> */
+/* #include <math.h> */
+/* #include <netinet/in.h> */
+/* #include <stdlib.h> */
+/* #include <string.h> */
+/* #include <sys/stat.h> */
+/* #include <sys/types.h> */
 
 /* Thread-safe wrapper around fprintf(stderr, ...) */
-#define print_error(repeater_, ...)                \
-  do {                                             \
-    pthread_mutex_lock(&repeater_->stderr_lock);   \
-    fprintf(stderr, __VA_ARGS__);                  \
-    pthread_mutex_unlock(&repeater_->stderr_lock); \
+#define brf_print(repeater_, ...)                    \
+  do {                                               \
+    pthread_mutex_lock(&(repeater_)->stderr_lock);   \
+    fprintf(stderr, __VA_ARGS__);                    \
+    pthread_mutex_unlock(&(repeater_)->stderr_lock); \
   } while (0)
-
-volatile bool do_exit = false;
 
 #define MAX_GAIN 60
 #define DEFAULT_GAIN 45
 
 volatile int rx_buf_offset;  // remember to initialize it!
 
-static bladerf_t s_blade;
+static brf_t s_brf;
 
 void btle_sigint_callback_handler(int signum) {
   fprintf(stdout, "Caught signal %d\n", signum);
-  do_exit = true;
 }
 
 uint64_t btle_get_freq_by_channel_number(int channel_number) {
@@ -54,7 +54,28 @@ uint64_t btle_get_freq_by_channel_number(int channel_number) {
   return (freq_hz);
 }
 
-void *btle_stream_callback(bladerf_t *dev, bladerf_stream_t *stream,
+void btle_demod_byte(IQ_TYPE *rxp, int num_byte, uint8_t *out_byte) {
+  int i, j;
+  int I0, Q0, I1, Q1;
+  uint8_t bit_decision;
+  int sample_idx = 0;
+
+  for (i = 0; i < num_byte; i++) {
+    out_byte[i] = 0;
+    for (j = 0; j < 8; j++) {
+      I0 = rxp[sample_idx];
+      Q0 = rxp[sample_idx + 1];
+      I1 = rxp[sample_idx + 2];
+      Q1 = rxp[sample_idx + 3];
+      bit_decision = (I0 * Q1 - I1 * Q0) > 0 ? 1 : 0;
+      out_byte[i] = out_byte[i] | (bit_decision << j);
+
+      sample_idx = sample_idx + SAMPLE_PER_SYMBOL * 2;
+    }
+  }
+}
+
+void *btle_stream_callback(struct bladerf *dev, struct bladerf_stream *stream,
                            struct bladerf_metadata *metadata, void *samples,
                            size_t num_samples, void *user_data) {
   (void)dev;
@@ -62,55 +83,61 @@ void *btle_stream_callback(bladerf_t *dev, bladerf_stream_t *stream,
   (void)metadata;
   (void)samples;
   (void)num_samples;
-  bladerf_t *blade = (bladerf_t *)user_data;
+  (void)user_data;
+  brf_t *brf = (brf_t *)user_data;
 
-  struct bladerf_async_task *tmp_p = &blade->async_task;
+  brf_print(&brf->async_task,
+            "btle_stream_callback: sample count expected(%x) got(%lx)\n",
+            SAMPLES_PER_BLOCK, num_samples);
 
-  int *sample = (int32_t *)samples;
-  for (size_t i = 0; i < num_samples; i++) {
-    int16_t Q = (((*sample) & 0xffff0000) >> 16);
-    int16_t I = (((*sample) & 0x0000ffff));
-    print_error(tmp_p, "Q(%d) I(%d)\n", Q, I);
+  brf_data_t *rx_data = &brf->rx_data;
+  memcpy(rx_data->rx_block_buf, samples, PER_BLOCK_BUF_SIZE);
+  rx_data->new_sample = true;
 
-    /* rx_buf[rx_buf_offset] = (((*sample) >> 4) & 0xFF); */
-    /* rx_buf[rx_buf_offset + 1] = (((*(sample + 1)) >> 4) & 0xFF); */
-    /* rx_buf_offset = (rx_buf_offset + 2) & (LEN_BUF - 1);  // cyclic buffer */
-
-    sample++;
+  if (brf->debug) {
+    return BLADERF_STREAM_SHUTDOWN;
   }
 
-  if (true || do_exit) {
-    return NULL;
+  if (brf->async_task.do_exit) {
+    return BLADERF_STREAM_SHUTDOWN;
   }
 
   return samples;
 }
 
+void btle_demod_byte(IQ_TYPE *rxp, int num_byte, uint8_t *out_byte) {
+  int *sample = (int32_t *)samples;
+  for (size_t i = 0; i < num_samples; i++) {
+    int16_t Q = (((*sample) & 0xffff0000) >> 16);
+    int16_t I = (((*sample) & 0x0000ffff));
+    /* rx_buf[rx_buf_offset] = (((*sample) >> 4) & 0xFF); */
+    /* rx_buf[rx_buf_offset + 1] = (((*(sample + 1)) >> 4) & 0xFF); */
+    /* rx_buf_offset = (rx_buf_offset + 2) & (LEN_BUF - 1);  // cyclic buffer */
+    /* sample++; */
+  }
+}
+
 void *btle_rx_task_run(void *ctx) {
   int status;
-  bladerf_t *blade = (bladerf_t *)ctx;
+  brf_t *brf = (brf_t *)ctx;
 
-  struct bladerf_async_task *tmp_p = &blade->async_task;
-
-  print_error(tmp_p, "Start RX bladerf stream.");
+  brf_print(&brf->async_task, "Start RX bladerf stream.\n");
 
   /* Start stream and stay there until we kill the stream */
-  status = bladerf_stream(blade->stream, BLADERF_MODULE_RX);
+  status = bladerf_stream(brf->stream, BLADERF_MODULE_RX);
   if (status < 0) {
-    print_error(tmp_p, "RX stream failure: %s\r\n", bladerf_strerror(status));
+    brf_print(&brf->async_task, "RX stream failure: %s\r\n",
+              bladerf_strerror(status));
   }
 
-  print_error(tmp_p, "Existing RX bladerf stream.");
+  brf_print(&brf->async_task, "Existing RX bladerf stream.\n");
+  s_brf.async_task.do_exit = true;
   return NULL;
 }
 
-int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
+int btle_setup_board(brf_t *brf, uint64_t freq_hz, int gain) {
   int status;
   unsigned int actual;
-
-  blade->rx_data.idx = 0;
-  blade->rx_data.num_buffers = 2;
-  blade->rx_data.samples_per_buffer = (LEN_BUF / 2);
 
   if (signal(SIGINT, btle_sigint_callback_handler) == SIG_ERR ||
       signal(SIGTERM, btle_sigint_callback_handler) == SIG_ERR) {
@@ -118,7 +145,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
     goto exit_failure;
   }
 
-  status = bladerf_open(&blade->rf_dev, NULL);
+  status = bladerf_open(&brf->rf_dev, NULL);
   if (status < 0) {
     fprintf(stderr, "Failed to open device: %s\n", bladerf_strerror(status));
     goto exit_failure;
@@ -126,7 +153,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
     fprintf(stdout, "open device: %s\n", bladerf_strerror(status));
   }
 
-  status = bladerf_is_fpga_configured(blade->rf_dev);
+  status = bladerf_is_fpga_configured(brf->rf_dev);
   if (status < 0) {
     fprintf(stderr, "Failed to determine FPGA state: %s\n",
             bladerf_strerror(status));
@@ -138,7 +165,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
     fprintf(stdout, "FPGA is loaded.\n");
   }
 
-  status = bladerf_set_frequency(blade->rf_dev, BLADERF_MODULE_RX, freq_hz);
+  status = bladerf_set_frequency(brf->rf_dev, BLADERF_MODULE_RX, freq_hz);
   if (status != 0) {
     fprintf(stderr, "Failed to set frequency: %s\n", bladerf_strerror(status));
     goto exit_close_blade;
@@ -147,7 +174,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
             bladerf_strerror(status));
   }
 
-  status = bladerf_set_sample_rate(blade->rf_dev, BLADERF_MODULE_RX,
+  status = bladerf_set_sample_rate(brf->rf_dev, BLADERF_MODULE_RX,
                                    SAMPLE_PER_SYMBOL * 1000000ul, &actual);
   if (status != 0) {
     fprintf(stderr, "Failed to set sample rate: %s\n",
@@ -158,7 +185,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
             bladerf_strerror(status));
   }
 
-  status = bladerf_set_bandwidth(blade->rf_dev, BLADERF_MODULE_RX,
+  status = bladerf_set_bandwidth(brf->rf_dev, BLADERF_MODULE_RX,
                                  SAMPLE_PER_SYMBOL * 1000000ul / 2, &actual);
   if (status != 0) {
     fprintf(stderr, "Failed to set bandwidth: %s\n", bladerf_strerror(status));
@@ -168,7 +195,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
             bladerf_strerror(status));
   }
 
-  status = bladerf_set_gain(blade->rf_dev, BLADERF_MODULE_RX, gain);
+  status = bladerf_set_gain(brf->rf_dev, BLADERF_MODULE_RX, gain);
   if (status != 0) {
     fprintf(stderr, "Failed to set gain: %s\n", bladerf_strerror(status));
     goto exit_close_blade;
@@ -177,7 +204,7 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
             bladerf_strerror(status));
   }
 
-  status = bladerf_get_gain(blade->rf_dev, BLADERF_MODULE_RX, &gain);
+  status = bladerf_get_gain(brf->rf_dev, BLADERF_MODULE_RX, &gain);
   if (status != 0) {
     fprintf(stderr, "Failed to get gain: %s\n", bladerf_strerror(status));
     goto exit_close_blade;
@@ -185,66 +212,74 @@ int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain) {
     fprintf(stdout, "bladerf_get_gain: %d %s\n", gain,
             bladerf_strerror(status));
   }
-
-  /* Initialize the stream */
-  status = bladerf_init_stream(
-      &blade->stream, blade->rf_dev, btle_stream_callback,
-      &blade->rx_data.buffers, blade->rx_data.num_buffers,
-      BLADERF_FORMAT_SC16_Q11, blade->rx_data.samples_per_buffer,
-      blade->rx_data.num_buffers, blade);
-
-  if (status != 0) {
-    fprintf(stderr, "Failed to init stream: %s\n", bladerf_strerror(status));
-    goto exit_close_blade;
-  } else {
-    fprintf(stdout, "init stream: %s\n", bladerf_strerror(status));
-  }
-
-  bladerf_set_stream_timeout(blade->rf_dev, BLADERF_RX, 100);
-
-  status = bladerf_enable_module(blade->rf_dev, BLADERF_MODULE_RX, true);
-  if (status < 0) {
-    fprintf(stderr, "Failed to enable module: %s\n", bladerf_strerror(status));
-    bladerf_deinit_stream(blade->stream);
-    goto exit_close_blade;
-  } else {
-    fprintf(stdout, "enable module true: %s\n", bladerf_strerror(status));
-  }
-
-  status = pthread_create(&(blade->async_task.rx_task), NULL, btle_rx_task_run,
-                          blade);
-  if (status < 0) {
-    goto exit_close_blade;
-  }
-
   return EXIT_SUCCESS;
 
 exit_close_blade:
-  if (blade->rf_dev) {
-    bladerf_close(blade->rf_dev);
+  if (brf->rf_dev) {
+    bladerf_close(brf->rf_dev);
   }
 
 exit_failure:
   return EXIT_FAILURE;
 }
 
-void btle_teardown_board(bladerf_t *blade) {
+void btle_teardown_board(brf_t *brf) {
   int status;
 
-  bladerf_deinit_stream(blade->stream);
-  printf("bladerf_deinit_stream.\n");
-
-  status = bladerf_enable_module(blade->rf_dev, BLADERF_MODULE_RX, false);
+  status = bladerf_enable_module(brf->rf_dev, BLADERF_MODULE_RX, false);
   if (status < 0) {
     fprintf(stderr, "Failed to enable module: %s\n", bladerf_strerror(status));
   } else {
     fprintf(stdout, "enable module false: %s\n", bladerf_strerror(status));
   }
 
-  bladerf_close(blade->rf_dev);
+  bladerf_close(brf->rf_dev);
   printf("bladerf_close.\n");
+}
 
-  pthread_join(blade->async_task.rx_task, NULL);
+int btle_setup_stream(brf_t *brf) {
+  int status;
+
+  /* Initialize the stream */
+  status = bladerf_init_stream(
+      &brf->stream, brf->rf_dev, btle_stream_callback, &brf->stream_buffers,
+      NUM_OF_BLOCKS, BLADERF_FORMAT_SC16_Q11, SAMPLES_PER_BLOCK, 1, brf);
+
+  if (status != 0) {
+    fprintf(stderr, "Failed to init stream: %s\n", bladerf_strerror(status));
+    goto exit_failure;
+  } else {
+    fprintf(stdout, "init stream: %s\n", bladerf_strerror(status));
+  }
+
+  bladerf_set_stream_timeout(brf->rf_dev, BLADERF_RX, 100);
+
+  status = bladerf_enable_module(brf->rf_dev, BLADERF_MODULE_RX, true);
+  if (status < 0) {
+    fprintf(stderr, "Failed to enable module: %s\n", bladerf_strerror(status));
+    bladerf_deinit_stream(brf->stream);
+    goto exit_failure;
+  } else {
+    fprintf(stdout, "enable module true: %s\n", bladerf_strerror(status));
+  }
+
+  status =
+      pthread_create(&(brf->async_task.rx_task), NULL, btle_rx_task_run, brf);
+  if (status < 0) {
+    goto exit_failure;
+  }
+
+  return EXIT_SUCCESS;
+
+exit_failure:
+  return EXIT_FAILURE;
+}
+
+void btle_teardown_stream(brf_t *brf) {
+  bladerf_deinit_stream(brf->stream);
+  printf("bladerf_deinit_stream.\n");
+
+  pthread_join(brf->async_task.rx_task, NULL);
   // pthread_cancel(async_task.rx_task);
   printf("bladeRF rx thread quit.\n");
 }
@@ -295,25 +330,35 @@ int main(int argc, char **argv) {
       gain, filename_pcap);
 
   // run cyclic recv in background
-  do_exit = false;
-  if (btle_setup_board(&s_blade, freq_hz, gain) != 0) {
-    if (s_blade.rf_dev != NULL) {
+  s_brf.async_task.do_exit = false;
+  if (btle_setup_board(&s_brf, freq_hz, gain) != EXIT_SUCCESS) {
+    if (s_brf.rf_dev != NULL) {
       goto program_quit;
     } else {
       return (1);
     }
   }
 
-  // scan
-  do_exit = false;
-  rx_buf_offset = 0;
-  while (do_exit == false) {
+  s_brf.debug = true;
+
+  if (btle_setup_stream(&s_brf) != EXIT_SUCCESS) {
+    goto program_quit;
   }
+
+  // scan
+  s_brf.async_task.do_exit = false;
+  rx_buf_offset = 0;
+  while (s_brf.async_task.do_exit == false) {
+    sleep(1);
+  }
+
+  printf("data read %d...\n", s_brf.rx_data.new_sample);
 
   goto program_quit;
 program_quit:
   printf("Exit main loop ...\n");
-  btle_teardown_board(&s_blade);
+  btle_teardown_stream(&s_brf);
+  btle_teardown_board(&s_brf);
 
   /* if (fh_pcap_store) fclose(fh_pcap_store); */
   return (0);
--- host/btle-tools/src/btle_blade.h
+++ host/btle-tools/src/btle_blade.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -28,52 +29,60 @@ enum {
 };
 /*----------------------------BTLE SPEC related---------------------------*/
 
-/*----------------------some basic signal definition----------------------*/
+typedef int16_t IQ_TYPE;
+typedef int32_t IQ_SAMPLE;
 
-#define LEN_BUF_IN_SAMPLE \
-  (4 * 4096)  // 4096 samples = ~1ms for 4Msps; ATTENTION each rx callback get
-              // hackrf.c:lib_device->buffer_size samples!!!
-#define LEN_BUF (LEN_BUF_IN_SAMPLE * 2)
-#define LEN_BUF_IN_SYMBOL (LEN_BUF_IN_SAMPLE / SAMPLE_PER_SYMBOL)
+/*----------------------Basic async stream buffer setup----------------------*/
+
+#define SAMPLES_PER_BLOCK 0x1000
+// 4096 samples = ~1ms for 4Msps; ATTENTION each rx callback get
+// hackrf.c:lib_device->buffer_size samples!!!
+
+#define PER_BLOCK_BUF_SIZE SAMPLES_PER_BLOCK * sizeof(IQ_SAMPLE)
+
+#define NUM_OF_BLOCKS 4
+
 #define MEGAHZ 1000000ull
 /*----------------------some basic signal definition----------------------*/
 
-typedef int8_t IQ_TYPE;
-
 /*----------------------BladeRF struct definition----------------------*/
+// Forard declaration
 struct bladerf_metadata;
-typedef struct bladerf_stream bladerf_stream_t;
-typedef struct bladerf bladerf_t;
+struct bladerf;
+struct bladerf_stream;
 
-typedef struct bladerf_async_task {
+typedef struct brf_async_task {
   pthread_t rx_task;
   pthread_mutex_t stderr_lock;
-} bladerf_async_task_t;
+  volatile bool do_exit;
+} brf_async_task_t;
 
-typedef struct bladerf_data {
-  void **buffers;            /* Transmit buffers */
-  size_t num_buffers;        /* Number of buffers */
-  size_t samples_per_buffer; /* Number of samples per buffer */
-  unsigned int idx;          /* The next one that needs to go out */
-  volatile IQ_TYPE rx_buf[LEN_BUF + LEN_BUF_MAX_NUM_PHY_SAMPLE];
-  //    bladerf_module      module;         /* Direction */
-  //    FILE                *fout;          /* Output file (RX only) */
-  //    ssize_t             samples_left;   /* Number of samples left */
-} bladerf_data_t;
+typedef struct brf_data {
+  unsigned char rx_block_buf[PER_BLOCK_BUF_SIZE];
+  bool new_sample;
+} brf_data_t;
 
-typedef struct bladerf {
-  bladerf_stream_t *stream;
-  bladerf_async_task_t async_task;
-  bladerf_data_t rx_data;
-  bladerf_t *rf_dev;
-} bladerf_t;
+typedef struct brf {
+  struct bladerf_stream *stream;
+  void **stream_buffers; /* Transmit buffers */
+
+  brf_async_task_t async_task;
+  brf_data_t rx_data;
+  struct bladerf *rf_dev;
+
+  bool debug;
+} brf_t;
 /*----------------------BladeRF struct definition----------------------*/
 
 uint64_t btle_get_freq_by_channel_number(int channel_number);
 
-int btle_setup_board(bladerf_t *blade, uint64_t freq_hz, int gain);
+int btle_setup_board(brf_t *brf, uint64_t freq_hz, int gain);
 
-void btle_teardown_board(bladerf_t *blade);
+void btle_teardown_board(brf_t *brf);
+
+int btle_setup_stream(brf_t *brf);
+
+void btle_teardown_stream(brf_t *brf);
 
 void btle_sigint_callback_handler(int signum);
 
@@ -82,6 +91,8 @@ void *btle_stream_callback(struct bladerf *dev, struct bladerf_stream *stream,
                            size_t num_samples, void *user_data);
 
 void *btle_rx_task_run(void *ctx);
+
+void btle_demod_byte(IQ_TYPE *rxp, int num_byte, uint8_t *out_byte);
 
 #ifdef __cplusplus
 }
