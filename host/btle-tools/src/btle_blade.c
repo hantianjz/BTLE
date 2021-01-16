e7d174d6cbb7724f3d4eb010a8380d33e759eeeb
James Zhao <hantian@google.com>
2021-01-15 22:45:28 -0500

    Some code clean up

--- host/btle-tools/src/btle_blade.c
+++ host/btle-tools/src/btle_blade.c
@@ -22,7 +22,8 @@
 #define brf_print(repeater_, ...)                    \
   do {                                               \
     pthread_mutex_lock(&(repeater_)->stderr_lock);   \
-    fprintf(stderr, __VA_ARGS__);                    \
+    fprintf(stdout, __VA_ARGS__);                    \
+    fflush(stdout);                                  \
     pthread_mutex_unlock(&(repeater_)->stderr_lock); \
   } while (0)
 
@@ -34,7 +35,8 @@ volatile int rx_buf_offset;  // remember to initialize it!
 static brf_t s_brf;
 
 void btle_sigint_callback_handler(int signum) {
-  fprintf(stdout, "Caught signal %d\n", signum);
+  fprintf(stderr, "\nCaught signal %d\n", signum);
+  s_brf.async_tasks.do_exit = true;
 }
 
 uint64_t btle_get_freq_by_channel_number(int channel_number) {
@@ -61,70 +63,201 @@ void *btle_stream_callback(struct bladerf *dev, struct bladerf_stream *stream,
   (void)dev;
   (void)stream;
   (void)metadata;
+  (void)num_samples;
   brf_t *brf = (brf_t *)user_data;
 
-  brf_print(&brf->async_task,
-            "btle_stream_callback: sample count expected(%x) got(%lx)\n",
-            SAMPLES_PER_BLOCK, num_samples);
+  /* brf_print(&brf->async_tasks, "%s: samples(%p) count expected(%x)
+   * got(%lx)\n", */
+  /*           __func__, samples, SAMPLES_PER_BLOCK, num_samples); */
 
-  brf_data_t *rx_data = &brf->rx_data;
-  memcpy(rx_data->rx_block_buf, samples, PER_BLOCK_BUF_SIZE);
-  rx_data->new_sample = true;
+  brf_data_t *rx_data = NULL;
+  for (unsigned i = 0; i < sizeof(brf->rx_data) / sizeof(brf->rx_data[0]);
+       i++) {
+    if (!brf->rx_data[i].new_sample) {
+      rx_data = &brf->rx_data[i];
+      break;
+    }
+  }
+
+  if (rx_data) {
+    pthread_mutex_lock(&rx_data->lock);
+    memcpy(rx_data->rx_block_buf, samples, PER_BLOCK_BUF_SIZE);
+    rx_data->new_sample = true;
+    pthread_mutex_unlock(&rx_data->lock);
+
+    pthread_cond_signal(&brf->async_tasks.rx_data_cond);
+  } else {
+    brf_print(&brf->async_tasks,
+              "btle_stream_callback: skipping samples due to low buffer.\n");
+  }
 
   if (brf->debug) {
+    brf->async_tasks.do_exit = true;
+  }
+
+  if (brf->async_tasks.do_exit) {
     return BLADERF_STREAM_SHUTDOWN;
   }
 
-  if (brf->async_task.do_exit) {
-    return BLADERF_STREAM_SHUTDOWN;
-  }
-
-  return samples;
+  brf->stream_buffers_idx = (brf->stream_buffers_idx + 1) % NUM_OF_BLOCKS;
+  return brf->stream_buffers[brf->stream_buffers_idx];
 }
 
-size_t btle_demod_byte(IQ_TYPE *iq, size_t num_samples, uint8_t *out_bytes,
-                       size_t out_bytes_size) {
-  uint8_t byte = 0;
-  size_t out_byte_idx = 0;
-  int out_bit_idx = 0;
+void brf_uint32_to_bit_array(uint32_t uint32_in, uint8_t *bit) {
+  for (size_t i = 0; i < sizeof(uint32_in) * NUM_BIT_PER_BYTE; i++) {
+    bit[i] = 0x01 & uint32_in;
+    uint32_in = (uint32_in >> 1);
+  }
+}
+
+void btle_print_xxd(uint8_t *data, size_t len) {
+  for (size_t i = 0; i < len; ++i) {
+    printf("%02x", data[i]);
+    if (i % 4 == 3) {
+      printf(" ");
+    }
+    if (i % 16 == 15) {
+      printf("\n");
+    }
+  }
+  printf("\n");
+}
+
+uint8_t *brf_search_bit_array_pattern(uint8_t *data, size_t data_len,
+                                      uint8_t *pattern, size_t pattern_len) {
+  for (size_t i = 0; i < (data_len - pattern_len); i++) {
+    if (!memcmp(&data[i], pattern, pattern_len)) {
+      return &data[i];
+    }
+  }
+  return NULL;
+}
+
+inline int search_unique_bits(IQ_TYPE *rxp, unsigned search_len,
+                              uint8_t *unique_bits, uint8_t *unique_bits_mask,
+                              const int num_bits) {
+  int sp, i0, q0, i1, q1, k, p, phase_idx;
+  bool unequal_flag;
+  const int demod_buf_len = num_bits;
+  int demod_buf_offset = 0;
+
+  uint8_t demod_buf_access[SAMPLE_PER_SYMBOL][LEN_DEMOD_BUF_ACCESS];
+
+  // demod_buf_preamble_access[SAMPLE_PER_SYMBOL][LEN_DEMOD_BUF_PREAMBLE_ACCESS]
+  // memset(demod_buf_preamble_access, 0,
+  // SAMPLE_PER_SYMBOL*LEN_DEMOD_BUF_PREAMBLE_ACCESS);
+  memset(demod_buf_access, 0, SAMPLE_PER_SYMBOL * LEN_DEMOD_BUF_ACCESS);
+  for (unsigned i = 0; i < search_len * SAMPLE_PER_SYMBOL * 2;
+       i = i + (SAMPLE_PER_SYMBOL * 2)) {
+    sp = ((demod_buf_offset - demod_buf_len + 1) & (demod_buf_len - 1));
+    // sp = (demod_buf_offset-demod_buf_len+1);
+    // if (sp>=demod_buf_len)
+    //  sp = sp - demod_buf_len;
+
+    for (unsigned j = 0; j < (SAMPLE_PER_SYMBOL * 2); j = j + 2) {
+      i0 = rxp[i + j];
+      q0 = rxp[i + j + 1];
+      i1 = rxp[i + j + 2];
+      q1 = rxp[i + j + 3];
+
+      phase_idx = j / 2;
+      // demod_buf_preamble_access[phase_idx][demod_buf_offset] = (i0*q1 -
+      // i1*q0) > 0? 1: 0;
+      demod_buf_access[phase_idx][demod_buf_offset] =
+          (i0 * q1 - i1 * q0) > 0 ? 1 : 0;
+
+      k = sp;
+      unequal_flag = false;
+      for (p = 0; p < demod_buf_len; p++) {
+        // if (demod_buf_preamble_access[phase_idx][k] != unique_bits[p]) {
+        if (demod_buf_access[phase_idx][k] != unique_bits[p] &&
+            unique_bits_mask[p]) {
+          unequal_flag = true;
+          break;
+        }
+        k = ((k + 1) & (demod_buf_len - 1));
+        // k = (k + 1);
+        // if (k>=demod_buf_len)
+        //  k = k - demod_buf_len;
+      }
+
+      if (unequal_flag == false) {
+        return (i + j - (demod_buf_len - 1) * SAMPLE_PER_SYMBOL * 2);
+      }
+    }
+
+    demod_buf_offset = ((demod_buf_offset + 1) & (demod_buf_len - 1));
+    // demod_buf_offset  = (demod_buf_offset+1);
+    // if (demod_buf_offset>=demod_buf_len)
+    //  demod_buf_offset = demod_buf_offset - demod_buf_len;
+  }
+
+  return (-1);
+}
+
+size_t btle_demod_bits(IQ_TYPE *iq, size_t num_samples, uint8_t *out_bits,
+                       size_t out_bit_size) {
+  size_t out_bit_idx = 0;
   size_t sample_idx = 0;
-  while (((sample_idx + 1) < num_samples) && (out_byte_idx < out_bytes_size)) {
+  while (((sample_idx + 1) < num_samples) && (out_bit_idx < out_bit_size)) {
     IQ_TYPE I0 = iq[sample_idx];
     IQ_TYPE Q0 = iq[sample_idx + 1];
     IQ_TYPE I1 = iq[sample_idx + 2];
     IQ_TYPE Q1 = iq[sample_idx + 3];
 
     uint8_t bit_decision = (I0 * Q1 - I1 * Q0) > 0 ? 1 : 0;
-    printf("I0(%d); Q0(%d); I1(%d); Q1(%d) == %d\n", I0, Q0, I1, Q1,
-           bit_decision);
-    byte = (byte << 1) | bit_decision;
+    /* printf("I0(%d); Q0(%d); I1(%d); Q1(%d) == %d\n", I0, Q0, I1, Q1, */
+    /*        bit_decision); */
+    out_bits[out_bit_idx] = bit_decision;
     out_bit_idx++;
-    if ((out_bit_idx % 8) == 0) {
-      out_bit_idx = 0;
-      out_bytes[out_byte_idx] = byte;
-      out_byte_idx++;
-    }
     sample_idx += 4;
   }
 
-  return out_byte_idx;
+  return out_bit_idx;
 }
 
 void *btle_rx_task_run(void *ctx) {
   int status;
   brf_t *brf = (brf_t *)ctx;
 
-  brf_print(&brf->async_task, "Start RX bladerf stream.\n");
+  brf_print(&brf->async_tasks, "Start RX bladerf stream.\n");
 
   /* Start stream and stay there until we kill the stream */
   status = bladerf_stream(brf->stream, BLADERF_MODULE_RX);
   if (status < 0) {
-    brf_print(&brf->async_task, "RX stream failure: %s\r\n",
+    brf_print(&brf->async_tasks, "RX stream failure: %s\r\n",
               bladerf_strerror(status));
   }
 
-  brf_print(&brf->async_task, "Existing RX bladerf stream.\n");
-  s_brf.async_task.do_exit = true;
+  brf_print(&brf->async_tasks, "Exiting RX bladerf stream: %d.\n", status);
+  return NULL;
+}
+
+void *btle_demod_task_run(void *ctx) {
+  brf_t *brf = (brf_t *)ctx;
+
+  while (!s_brf.async_tasks.do_exit) {
+    brf_data_t *rx_data = NULL;
+    for (unsigned i = 0; i < sizeof(brf->rx_data) / sizeof(brf->rx_data[0]);
+         i++) {
+      if (brf->rx_data[i].new_sample) {
+        rx_data = &brf->rx_data[i];
+        break;
+      }
+    }
+
+    if (!rx_data) {
+      pthread_cond_wait(&brf->async_tasks.rx_data_cond,
+                        &brf->async_tasks.rx_data_lock);
+    } else {
+      pthread_mutex_lock(&rx_data->lock);
+      btle_demod_process(brf, rx_data);
+      rx_data->new_sample = false;
+      pthread_mutex_unlock(&rx_data->lock);
+    }
+  }
+
+  brf_print(&brf->async_tasks, "Exiting RX demod task\n");
   return NULL;
 }
 
@@ -237,6 +370,7 @@ int btle_setup_stream(brf_t *brf) {
   status = bladerf_init_stream(
       &brf->stream, brf->rf_dev, btle_stream_callback, &brf->stream_buffers,
       NUM_OF_BLOCKS, BLADERF_FORMAT_SC16_Q11, SAMPLES_PER_BLOCK, 1, brf);
+  brf->stream_buffers_idx = 0;
 
   if (status != 0) {
     fprintf(stderr, "Failed to init stream: %s\n", bladerf_strerror(status));
@@ -257,7 +391,7 @@ int btle_setup_stream(brf_t *brf) {
   }
 
   status =
-      pthread_create(&(brf->async_task.rx_task), NULL, btle_rx_task_run, brf);
+      pthread_create(&(brf->async_tasks.rx_task), NULL, btle_rx_task_run, brf);
   if (status < 0) {
     goto exit_failure;
   }
@@ -269,12 +403,56 @@ exit_failure:
 }
 
 void btle_teardown_stream(brf_t *brf) {
+  pthread_join(brf->async_tasks.demod_task, NULL);
+  pthread_join(brf->async_tasks.rx_task, NULL);
+
   bladerf_deinit_stream(brf->stream);
   printf("bladerf_deinit_stream.\n");
 
-  pthread_join(brf->async_task.rx_task, NULL);
-  // pthread_cancel(async_task.rx_task);
+  // pthread_cancel(async_tasks.rx_task);
   printf("bladeRF rx thread quit.\n");
+}
+
+int btle_setup_demod(brf_t *brf) {
+  pthread_cond_init(&brf->async_tasks.rx_data_cond, NULL);
+
+  int status = pthread_create(&(brf->async_tasks.demod_task), NULL,
+                              btle_demod_task_run, brf);
+  if (status < 0) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
+void btle_demod_process(brf_t *brf, brf_data_t *rx_data) {
+  int num_symbol_left =
+      sizeof(rx_data->rx_block_buf) / (SAMPLE_PER_SYMBOL * 2);  // 2 for IQ
+  int ret =
+      search_unique_bits((IQ_TYPE *)rx_data->rx_block_buf, num_symbol_left,
+                         brf->access_addr_bit_array, brf->access_mask_bit_array,
+                         LEN_DEMOD_BUF_ACCESS);
+  if (ret > 0) {
+    brf_print(&brf->async_tasks, "+");
+  } else {
+    brf_print(&brf->async_tasks, ".");
+  }
+
+  /*
+  uint8_t demod_bits[SAMPLES_PER_BLOCK / SAMPLE_PER_SYMBOL];
+
+  brf_print(&brf->async_tasks, ".");
+  size_t demod_bits_count =
+      btle_demod_bits((IQ_TYPE *)rx_data->rx_block_buf, SAMPLES_PER_BLOCK,
+                      demod_bits, sizeof(demod_bits));
+
+  uint8_t *aligned_bits = brf_search_bit_array_pattern(
+      demod_bits, demod_bits_count, brf->access_addr_bit_array,
+      sizeof(brf->access_addr_bit_array));
+  if (aligned_bits) {
+    brf_print(&brf->async_tasks, "Found bits at (%p) bytes offset (%lu)\n",
+              aligned_bits, aligned_bits - demod_bits);
+  }
+  */
 }
 
 int main(int argc, char **argv) {
@@ -323,7 +501,7 @@ int main(int argc, char **argv) {
       gain, filename_pcap);
 
   // run cyclic recv in background
-  s_brf.async_task.do_exit = false;
+  s_brf.async_tasks.do_exit = false;
   if (btle_setup_board(&s_brf, freq_hz, gain) != EXIT_SUCCESS) {
     if (s_brf.rf_dev != NULL) {
       goto program_quit;
@@ -332,24 +510,31 @@ int main(int argc, char **argv) {
     }
   }
 
-  s_brf.debug = true;
+  s_brf.debug = false;
+
+  brf_uint32_to_bit_array(DEFAULT_ACCESS_ADDR, s_brf.access_addr_bit_array);
+  btle_print_xxd(s_brf.access_addr_bit_array,
+                 sizeof(s_brf.access_addr_bit_array));
+
+  brf_uint32_to_bit_array(DEFAULT_ACCESS_MASK, s_brf.access_mask_bit_array);
+  btle_print_xxd(s_brf.access_mask_bit_array,
+                 sizeof(s_brf.access_mask_bit_array));
 
   if (btle_setup_stream(&s_brf) != EXIT_SUCCESS) {
     goto program_quit;
   }
 
+  if (btle_setup_demod(&s_brf) != EXIT_SUCCESS) {
+    goto program_quit;
+  }
+
   // scan
-  s_brf.async_task.do_exit = false;
+  s_brf.async_tasks.do_exit = false;
   rx_buf_offset = 0;
-  while (s_brf.async_task.do_exit == false) {
+  while (s_brf.async_tasks.do_exit == false) {
     sleep(1);
   }
 
-  printf("data read %d...\n", s_brf.rx_data.new_sample);
-  btle_demod_byte((IQ_TYPE *)s_brf.rx_data.rx_block_buf, SAMPLES_PER_BLOCK,
-                  NULL, 0);
-
-  goto program_quit;
 program_quit:
   printf("Exit main loop ...\n");
   btle_teardown_stream(&s_brf);
--- host/btle-tools/src/btle_blade.h
+++ host/btle-tools/src/btle_blade.h
@@ -13,6 +13,7 @@ extern "C" {
 enum {
   DEFAULT_CHANNEL = 37,
   DEFAULT_ACCESS_ADDR = (0x8E89BED6),
+  DEFAULT_ACCESS_MASK = (0xFFFFFFFF),
   DEFAULT_CRC_INIT = (0x555555),
   MAX_CHANNEL_NUMBER = 39,
   MAX_NUM_INFO_BYTE = (43),
@@ -23,10 +24,13 @@ enum {
   MAX_NUM_PHY_SAMPLE = (MAX_NUM_PHY_BYTE * 8 * SAMPLE_PER_SYMBOL),
   LEN_BUF_MAX_NUM_PHY_SAMPLE = (2 * MAX_NUM_PHY_SAMPLE),
 
+  NUM_BIT_PER_BYTE = 8,
   NUM_PREAMBLE_BYTE = (1),
   NUM_ACCESS_ADDR_BYTE = (4),
   NUM_PREAMBLE_ACCESS_BYTE = (NUM_PREAMBLE_BYTE + NUM_ACCESS_ADDR_BYTE),
+  BRF_BLE_ACCESS_BYTES = 0x8E89BED6,  // {0xD6, 0xBE, 0x89, 0x8E};
 };
+
 /*----------------------------BTLE SPEC related---------------------------*/
 
 typedef int16_t IQ_TYPE;
@@ -34,13 +38,12 @@ typedef int32_t IQ_SAMPLE;
 
 /*----------------------Basic async stream buffer setup----------------------*/
 
-#define SAMPLES_PER_BLOCK 0x1000
-// 4096 samples = ~1ms for 4Msps; ATTENTION each rx callback get
-// hackrf.c:lib_device->buffer_size samples!!!
-
-#define PER_BLOCK_BUF_SIZE SAMPLES_PER_BLOCK * sizeof(IQ_SAMPLE)
-
-#define NUM_OF_BLOCKS 4
+enum {
+  SAMPLES_PER_BLOCK = (4 * 0x1000),
+  PER_BLOCK_BUF_SIZE = (SAMPLES_PER_BLOCK * sizeof(IQ_SAMPLE)),
+  NUM_OF_BLOCKS = 4,
+  LEN_DEMOD_BUF_ACCESS = (NUM_ACCESS_ADDR_BYTE * 8),  // 32 = 2^5
+};
 
 #define MEGAHZ 1000000ull
 /*----------------------some basic signal definition----------------------*/
@@ -53,24 +56,41 @@ struct bladerf_stream;
 
 typedef struct brf_async_task {
   pthread_t rx_task;
+  pthread_t demod_task;
+
+  pthread_cond_t rx_data_cond;
+  pthread_mutex_t rx_data_lock;
+
   pthread_mutex_t stderr_lock;
   volatile bool do_exit;
 } brf_async_task_t;
 
 typedef struct brf_data {
   unsigned char rx_block_buf[PER_BLOCK_BUF_SIZE];
+  pthread_mutex_t lock;
   bool new_sample;
 } brf_data_t;
 
 typedef struct brf {
   struct bladerf_stream *stream;
-  void **stream_buffers; /* Transmit buffers */
 
-  brf_async_task_t async_task;
-  brf_data_t rx_data;
+  /* Transmit buffers are allocated by bladerf_init_stream().
+   * It is defined as an array of pointers of len NUM_OF_BLOCKS
+   * where each pointer in the array point to a block of memory of size
+   * PER_BLOCK_BUF_SIZE. Hence it is helpful it keep track of a idx value
+   * to keep track which one we currently using.
+   * */
+  void **stream_buffers;
+  int stream_buffers_idx;
+
+  brf_async_task_t async_tasks;
+  brf_data_t rx_data[NUM_OF_BLOCKS];
   struct bladerf *rf_dev;
 
   bool debug;
+
+  uint8_t access_addr_bit_array[sizeof(uint32_t) * NUM_BIT_PER_BYTE];
+  uint8_t access_mask_bit_array[sizeof(uint32_t) * NUM_BIT_PER_BYTE];
 } brf_t;
 /*----------------------BladeRF struct definition----------------------*/
 
@@ -84,16 +104,29 @@ int btle_setup_stream(brf_t *brf);
 
 void btle_teardown_stream(brf_t *brf);
 
+int btle_setup_demod(brf_t *brf);
+
 void btle_sigint_callback_handler(int signum);
 
 void *btle_stream_callback(struct bladerf *dev, struct bladerf_stream *stream,
                            struct bladerf_metadata *metadata, void *samples,
                            size_t num_samples, void *user_data);
 
+void brf_uint32_to_bit_array(uint32_t uint32_in, uint8_t *bit);
+
+void btle_print_xxd(uint8_t *data, size_t len);
+
+uint8_t *brf_search_bit_array_pattern(uint8_t *data, size_t data_len,
+                                      uint8_t *pattern, size_t pattern_len);
+
 void *btle_rx_task_run(void *ctx);
 
-size_t btle_demod_byte(IQ_TYPE *samples, size_t num_samples, uint8_t *out_bytes,
-                       size_t out_bytes_size);
+void *btle_demod_task_run(void *ctx);
+
+size_t btle_demod_bits(IQ_TYPE *samples, size_t num_samples, uint8_t *out_bits,
+                       size_t out_bit_size);
+
+void btle_demod_process(brf_t *brf, brf_data_t *data);
 
 #ifdef __cplusplus
 }
